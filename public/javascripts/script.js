e591a610ec560ca1c652f009a2bb415a19a35d56
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-19 18:33:26 +0000

    Use user input to make api request and  populate table

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 
-  $.get('/api/data?groupby=userRating', function(data){
-    var increment = 0
-    var query = 'userRating'
-    $("#count-table").append("<tr id='header'></tr>")
-    $("#header").append("<th>UserRating</th>")
-    $("#header").append("<th>Quantity</th>")
-    data.forEach(function(row){
-      $("#count-table").append("<tr id='row-" + increment + "'></tr>")
-      $("#row-" + increment).append("<th>" + row[query] + "</th>")
-      $("#row-" + increment).append("<th>" + row.count +"</th>")
-      increment++
-    })
-  });
+  $("#submit").click(function(){
+    var value = $("#query").val()
+    getData(value)
+  })
+
+  function getData(query){
+    $("#count-table").html('')
+    $.get('/api/data?groupby=' + query , function(data){
+      var increment = 0
+      $("#count-table").append("<tr id='header'></tr>")
+      $("#header").append("<th>UserRating</th>")
+      $("#header").append("<th>Quantity</th>")
+      data.forEach(function(row){
+        $("#count-table").append("<tr id='row-" + increment + "'></tr>")
+        $("#row-" + increment).append("<th>" + row[query] + "</th>")
+        $("#row-" + increment).append("<th>" + row.count +"</th>")
+        increment++
+      })
+    });
+  }
 });

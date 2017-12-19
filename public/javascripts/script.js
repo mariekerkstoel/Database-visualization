0af5435ee1a37764c6d9bc2515c159d580834946
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-19 17:57:02 +0000

    Add table, add ajax request to api, populate table with data from api

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 
   $.get('/api/data?groupby=userRating', function(data){
-    console.log(data);
+    var increment = 0
+    var query = 'userRating'
+    $("#count-table").append("<tr id='header'></tr>")
+    $("#header").append("<th>UserRating</th>")
+    $("#header").append("<th>Quantity</th>")
+    data.forEach(function(row){
+      $("#count-table").append("<tr id='row-" + increment + "'></tr>")
+      $("#row-" + increment).append("<th>" + row[query] + "</th>")
+      $("#row-" + increment).append("<th>" + row.count +"</th>")
+      increment++
+    })
   });
 });

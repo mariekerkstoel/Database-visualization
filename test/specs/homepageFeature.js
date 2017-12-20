be0ce80e88d777e1cd5352788da9e37eaaa453e7
antonio <antoniobelmardacosta@gmail.com>
2017-12-20 10:19:50 +0000

    Solve conflicts created from a previous merge

--- test/specs/homepageFeature.js
+++ test/specs/homepageFeature.js
@@ -4,13 +4,7 @@ var expect = chai.expect;
 describe('homepage', function() {
   it("hello worlds", function(done) {
     browser.url('http://localhost:3000/');
-    expect(browser.getText('body')).to.equal("Hello World")
-    browser.call(done);
-  });
-  it('should demonstrate click button', function(done){
-    browser.url('http://localhost:3000/')
-    browser.click('#button');
-    expect(browser.getLocation()).to.equal('http://localhost:3000/new/page')
+    expect(browser.getText('body')).to.equal("Submit")
     browser.call(done);
   });
 });

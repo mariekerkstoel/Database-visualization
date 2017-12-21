3780e1be29e095193976196f362f4893eff445de
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-21 10:58:12 +0000

    database seeding fix

--- seed.js
+++ seed.js
@@ -7,35 +7,35 @@ const sequelize = new Sequelize('data_vis_development', null, null, {
 
 var Movie = require('./models/movie')(sequelize);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
 
-Movie.sync();
-
-csv
-.fromPath("ratings.csv")
-.on("data", function(data) {
-  Movie.create({
-    userRating: data[1],
-    dateAdded: data[2],
-    title: data[3],
-    url: data[4],
-    titleType: data[5],
-    imdbRating: data[6],
-    runtimeInMins: data[7],
-    year: data[8],
-    genre: data[9],
-    numVotes: data[10],
-    releaseDate: data[11],
-    directors: data[12]
+Movie.sync({force: true}).then(function(){
+  csv
+  .fromPath("ratings.csv")
+  .on("data", function(data) {
+    Movie.create({
+      userRating: data[1],
+      dateAdded: data[2],
+      title: data[3],
+      url: data[4],
+      titleType: data[5],
+      imdbRating: data[6],
+      runtimeInMins: data[7],
+      year: data[8],
+      genre: data[9],
+      numVotes: data[10],
+      releaseDate: data[11],
+      directors: data[12]
+    })
   })
-})
-.on("end", function() {
-  console.log("done");
+  .on("end", function() {
+    console.log("done");
+  })
 })

f9915c7e082bbd8c1e865c0f6e845b703232e3c7
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-22 15:03:07 +0000

    Add click button tests

--- test/specs/homepageFeature.js
+++ test/specs/homepageFeature.js
@@ -2,9 +2,16 @@ var chai = require("chai");
 var expect = chai.expect;
 
 describe('homepage', function() {
-  it("hello worlds", function(done) {
+  it("prompts user with dropdown columns", function(done) {
     browser.url('http://localhost:3000/');
-    expect(browser.getText('body')).to.equal("Choose your table\nmovie\nsong\nBarchart\nTable\nSubmit")
+    expect(browser.getText('body')).to.equal("Data Visualisation\nChoose your table\nmovie\nsong\nChoose your attribute\nChoose your chart type\nBar chart\nPie chart\nTable\nSubmit");
+    browser.call(done);
+  });
+  it('Creates a table that user chooses', function(done){
+    browser.url('http://localhost:3000/');
+    browser.selectByValue('#model','movie');
+    browser.selectByValue('#groupby','titleType');
+    browser.selectByValue('#typeOfGraph','table');
     browser.call(done);
   });
 });

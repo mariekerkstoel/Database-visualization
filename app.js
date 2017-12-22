691909a9138c28615fde62c122e6f36ed57df7ea
antonio <antoniobelmardacosta@gmail.com>
2017-12-22 11:57:25 +0000

    Refactor app.js so that uses the Sequelize connection from db.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var app = express();
-var Sequelize = require('sequelize');
 var csv = require('fast-csv');
 var pluralize = require('pluralize')
 var path = require('path');
 var fs = require('fs');
 var directoryPath = path.join(__dirname, 'models');
-var sequelize = new Sequelize('data_vis_development', null, null, {
-  dialect: 'postgres'
-})
+var db = require('./config/db.js')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -35,10 +32,10 @@ app.get('/', function(req, res){
 app.get('/api/data', function(req, res){
   var model = req.query.model
   var query = req.query.groupby
-  var Model = require(`./models/${model}`)(sequelize, Sequelize)
+  var Model = require(`./models/${model}`)(db.sequelize, db.Sequelize)
   if (query) {
     Model.findAll({
-    attributes: [query, [sequelize.fn('COUNT', query ), 'count']],
+    attributes: [query, [db.sequelize.fn('COUNT', query ), 'count']],
     group: query,
     order: [
           [query, 'ASC']
@@ -62,7 +59,7 @@ app.get('/api/data/tables', function(req, res){
 app.get('/api/data/columns', function(req, res){
   var table = pluralize(req.query.model)
   var columnList = []
-  sequelize.query(`SELECT column_name FROM information_schema.columns WHERE table_name = '${table}' AND column_name!='id'AND column_name!='updatedAt' AND column_name!='createdAt';`).then(function(data) {
+  db.sequelize.query(`SELECT column_name FROM information_schema.columns WHERE table_name = '${table}' AND column_name!='id'AND column_name!='updatedAt' AND column_name!='createdAt';`).then(function(data) {
     data[0].forEach(function(hash){
      columnList.push(hash.column_name)
     })
@@ -70,7 +67,7 @@ app.get('/api/data/columns', function(req, res){
   });
 });
 
-sequelize
+db.sequelize
 .authenticate()
 .then(function() {
   console.log('Connection has been established successfully');
